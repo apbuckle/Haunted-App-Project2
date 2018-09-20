7a9e756f7ec1d9f855c3a8b6a3d860bce1ff206e
Alan <alanbuckle05@gmail.com>
2018-09-20 13:06:06 -0400

    rendering venues page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true })
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const venueRouter = require('./routes/venue')
 
 const app = express()
 
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/venue', venueRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
